ebe3dfe8a002653a9c2b220fc37da297892a6248
Jochen Diekenbrock <jodi-github@jochendiekenbrock.de>
2018-09-27 22:52:53 +0200

    use ImageComparison

--- example/example.spec.ts
+++ example/example.spec.ts
@@ -1,26 +1,19 @@
-import { browser, by, element } from "protractor";
+import { browser, by, element } from 'protractor';
+import { ImageComparison } from '../src';
 
-describe("angularjs homepage todo list", () => {
-    it("should add a todo", () => {
-        browser.get("https://angularjs.org");
+describe('angularjs homepage todo list', () => {
+    it('should add a todo', async () => {
+        browser.get('https://angularjs.org');
 
-        expect(
-            browser.protractorImageComparison.checkScreen("startPage")
-        ).toEqual(0);
+        const imageComparison = new ImageComparison();
+        await imageComparison.checkScreen('startPage');
 
-        element(by.model("todoList.todoText")).sendKeys(
-            "write first protractor test"
-        );
+        element(by.model('todoList.todoText')).sendKeys('write first protractor test');
         element(by.css("[value='add']")).click();
 
-        expect(
-            browser.protractorImageComparison.checkElement(
-                element(by.model("todoList.todoText")),
-                "anElement"
-            )
-        ).toEqual(0);
+        await imageComparison.checkElement(element(by.model('todoList.todoText')), 'anElement');
 
-        const todoList = element.all(by.repeater("todo in todoList.todos"));
+        const todoList = element.all(by.repeater('todo in todoList.todos'));
         expect(todoList.count()).toEqual(3);
     });
 });
