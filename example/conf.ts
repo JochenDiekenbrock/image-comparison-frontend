cf808cfeaa24c90e59f22bb032f82014514fcaf7
Jochen Diekenbrock <jodi-github@jochendiekenbrock.de>
2018-09-27 22:52:43 +0200

    format

--- example/conf.ts
+++ example/conf.ts
@@ -1,14 +1,15 @@
-import { browser, Config } from "protractor";
+import { browser, Config } from 'protractor';
 
 export const config: Config = {
     directConnect: true,
-    specs: ["example.spec.js"],
+    specs: ['example.spec.js'],
     onPrepare() {
-        const protractorImageComparison = require("protractor-image-comparison");
+        const protractorImageComparison = require('protractor-image-comparison');
         browser.protractorImageComparison = new protractorImageComparison({
             autoSaveBaseline: true,
-            baselineFolder: "dist/example/baseline/",
-            screenshotPath: "dist/example/"
+            baselineFolder: 'dist/example/baseline/',
+            screenshotPath: 'dist/example/',
+
         });
     }
 };
