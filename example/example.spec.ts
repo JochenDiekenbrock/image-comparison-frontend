270bdb6743e2bd1d9e9ebee8ce9e702a2eb57c7a
Jochen Diekenbrock <jodi-github@jochendiekenbrock.de>
2018-09-30 23:39:30 +0200

    minimize example test

--- example/example.spec.ts
+++ example/example.spec.ts
@@ -12,8 +12,5 @@ describe('angularjs homepage todo list', () => {
         element(by.css("[value='add']")).click();
 
         await imageComparison.checkElement(element(by.model('todoList.todoText')), 'anElement');
-
-        const todoList = element.all(by.repeater('todo in todoList.todos'));
-        expect(todoList.count()).toEqual(3);
     });
 });
