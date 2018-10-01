bd43a0d9580ae4d6f913d5e5bf969eacf3a6fb9a
Jochen Diekenbrock <jodi-github@jochendiekenbrock.de>
2018-10-01 23:20:37 +0200

    clean up the build

--- src/example/example.spec.ts
+++ src/example/example.spec.ts
@@ -1,5 +1,6 @@
 import { browser, by, element } from 'protractor';
-import { ImageComparison } from '../src';
+
+import { ImageComparison } from '../';
 
 describe('angularjs homepage todo list', () => {
     it('should add a todo', async () => {
@@ -9,7 +10,7 @@ describe('angularjs homepage todo list', () => {
         await imageComparison.checkScreen('startPage');
 
         element(by.model('todoList.todoText')).sendKeys('write first protractor test');
-        element(by.css("[value='add']")).click();
+        // element(by.css('[value="add"]')).click();
 
         await imageComparison.checkElement(element(by.model('todoList.todoText')), 'anElement');
     });
