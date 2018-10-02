df7d9cbba46c79bcabf4fa8f58a53768bd5c5550
Jochen Diekenbrock <jodi-github@jochendiekenbrock.de>
2018-10-02 13:55:55 +0200

    don't depend on ProtractorImageComparisonPaths

--- src/result.ts
+++ src/result.ts
@@ -1,6 +1,6 @@
 import { ProtractorImageComparisonPaths } from './protractor-image-comparison-paths';
 
-export interface Result extends ProtractorImageComparisonPaths {
+export interface Result {
     /* true, if the image comparison mismatch was below browser.protractorImageComparison.saveAboveTolerance */
     success: boolean;
 
@@ -8,4 +8,13 @@ export interface Result extends ProtractorImageComparisonPaths {
     date: Date;
 
     testName: string;
+
+    /* if the test encountered a difference between baselintImage and actualImage, this image highlights the
+       differences. Path is relativ to the current directory
+       */
+    imageDiffPath?: string;
+    /* the baseline image that is the reference for the image comparison. Path is relativ to the current directory */
+    baselineImage: string;
+    /* the image that was created during this test run. Path is relativ to the current directory */
+    actualImage: string;
 }
