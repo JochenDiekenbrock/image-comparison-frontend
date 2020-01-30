77a94709629d4873bc7eb7d4c134c9345a37c147
Jochen Diekenbrock <1625222+JochenDiekenbrock@users.noreply.github.com>
2020-01-30 07:49:28 +0100

    format and lint fixes

--- dist/image-comparison.d.ts
+++ dist/image-comparison.d.ts
@@ -4,8 +4,12 @@ export declare class ImageComparison {
     private readonly config;
     private pic;
     private reporter;
-    constructor(config?: Partial<Config>);
-    checkPage(testName: string, protractorImageComparisonOptions?: any): Promise<number>;
-    checkElement(elementFinder: ElementFinder, testName: string, protractorImageComparisonOptions?: {}): Promise<number>;
     private check;
+    constructor(config?: Partial<Config>);
+    public checkPage(testName: string, protractorImageComparisonOptions?: any): Promise<number>;
+    public checkElement(
+        elementFinder: ElementFinder,
+        testName: string,
+        protractorImageComparisonOptions?: {}
+    ): Promise<number>;
 }
